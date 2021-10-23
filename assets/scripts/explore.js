2e4954de4605c5b17a6b469685ff6fd8b3642fac
hgnguyenucsd <hgnguyen@ucsd.edu>
2021-10-22 17:09:41 -0700

    Finished Javascript portion of lab.

--- assets/scripts/explore.js
+++ assets/scripts/explore.js
@@ -1,7 +1,71 @@
 // explore.js
 
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", init);
+
+const voiceSelect = document.querySelector("#voice-select");
+const text = document.getElementById("text-to-speak");
+const button = document.querySelector("#explore > button");
+const picture = document.querySelector("#explore > img");
+let selectedOption = "";
+let utterance = new SpeechSynthesisUtterance("");
+let voices = [];
 
 function init() {
   // TODO
+
+  // This block of code came from MDN Web Docs for SpeechSynthesis with some very slight modifications.
+  // Its purpose is to extract all the possible voices and add them as options to a selection list on load.
+  function populateVoiceList() {
+    voices = speechSynthesis.getVoices();
+
+    for (var i = 0; i < voices.length; i++) {
+      var option = document.createElement("option");
+      option.textContent = voices[i].name + " (" + voices[i].lang + ")";
+
+      if (voices[i].default) {
+        option.textContent += " -- DEFAULT";
+      }
+
+      option.setAttribute("data-lang", voices[i].lang);
+      option.setAttribute("data-name", voices[i].name);
+      voiceSelect.appendChild(option);
+    }
+  }
+
+  populateVoiceList();
+
+  if (speechSynthesis.onvoiceschanged !== undefined) {
+    speechSynthesis.onvoiceschanged = populateVoiceList;
+  }
+
+  // Setting the text to be uttered to the value being inputted in the text area.
+  text.addEventListener("input", (event) => {
+    utterance.text = event.target.value;
+  });
+
+  // This block of code came from MDN Web Docs for SpeechSynthesis with some slight modifications.
+  // Its purpose is to go through all of the voices from the list that we made and look for the correct voice to use from the selection.
+  voiceSelect.addEventListener("change", (event) => {
+    selectedOption = voiceSelect.selectedOptions[0].getAttribute("data-name");
+    console.log(selectedOption);
+    for (var i = 0; i < voices.length; i++) {
+      if (voices[i].name === selectedOption) {
+        utterance.voice = voices[i];
+      }
+    }
+  });
+
+  // Clicking the "Press to Talk" button will cause the text to be said and the face icon changes accordingly to that.
+  button.addEventListener("click", (event) => {
+    if (selectedOption != "") {
+      button.onclick = speechSynthesis.speak(utterance);
+    }
+
+    utterance.addEventListener("start", (event) => {
+      picture.src = "assets/images/smiling-open.png";
+    });
+    utterance.addEventListener("end", (event) => {
+      picture.src = "assets/images/smiling.png";
+    });
+  });
 }
--- assets/scripts/expose.js
+++ assets/scripts/expose.js
@@ -1,7 +1,62 @@
 // expose.js
 
-window.addEventListener('DOMContentLoaded', init);
+window.addEventListener("DOMContentLoaded", init);
 
 function init() {
   // TODO
+
+  // For selecting the correct horn image & horn sound.
+  const selectHorn = document.getElementById("horn-select");
+  const image = document.querySelector("#expose > img");
+  const audio = document.querySelector(".hidden");
+  let selected = "";
+
+  // For playing the audio and confetti for the party horn.
+  const button = document.querySelector("#expose button");
+  const jsConfetti = new JSConfetti();
+
+  // For volume control icon and actual volume level.
+  const volume_icon = document.querySelector("#volume-controls > img");
+  let level = "";
+
+  // Event Listener for setting the correct image and audio upon selection.
+  selectHorn.addEventListener("change", (event) => {
+    selected = event.target.value;
+    if (selected === "air-horn") {
+      image.src = "assets/images/air-horn.svg";
+      audio.src = "assets/audio/air-horn.mp3";
+    } else if (selected === "party-horn") {
+      image.src = "assets/images/party-horn.svg";
+      audio.src = "assets/audio/party-horn.mp3";
+    } else if (selected === "car-horn") {
+      image.src = "assets/images/car-horn.svg";
+      audio.src = "assets/audio/car-horn.mp3";
+    }
+  });
+
+  // Event Listener for playing the audio and adding confetti for the party horn.
+  button.addEventListener("click", (event) => {
+    button.onclick = audio.play();
+
+    if (selected === "party-horn") {
+      jsConfetti.addConfetti();
+    }
+  });
+
+  // Event Listener for sound adjustion and sound icon.
+  volume.addEventListener("input", (event) => {
+    level = event.target.value;
+    audio.volume = event.target.value * 0.01;
+    if (level == 0) {
+      volume_icon.src = "assets/icons/volume-level-0.svg";
+    } else if (level > 0 && level < 33) {
+      volume_icon.src = "assets/icons/volume-level-1.svg";
+    } else if (level > 32 && level < 67) {
+      volume_icon.src = "assets/icons/volume-level-2.svg";
+    } else {
+      volume_icon.src = "assets/icons/volume-level-3.svg";
+    }
+  });
 }
+
+// Note: Picture sizes readjust weirdly.
